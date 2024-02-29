82d9ceefe46e1f25f05459ac8b63c703c2459b58
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-02-28 19:25:39 -0600

    Initial Commit

---
+++ TableViewVisualAppTabitaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewVisualAppTabitaS
+//
+//  Created by Tabita Sadiq on 2/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
